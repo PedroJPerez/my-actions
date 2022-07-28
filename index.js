435bfe5d140b4c883276b4a87970421e1dbe85eb
Jonathan Perez <pjperez1@asu.edu>
2022-07-27 23:33:13 -0700

    add pr comment

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ const github = require('@actions/github');
         try {
             core.notice("Calling this action");
             core.notice(github.context.issue.number);
+
+            const myToken = core.getInput('githubToken');
+
+            const octokit = github.getOctokit(myToken);
+
+            let response = await octokit.rest.issues.createComment(
+                {
+                issue_number: github.context.issue.number, 
+                body: `This is a test for PR ${github.context.issue.number}`, 
+                owner: github.context.owner,
+                repo: github.context.repo
+            });
+            
         } catch (error) {
             core.setFailed(error.message);
         }
